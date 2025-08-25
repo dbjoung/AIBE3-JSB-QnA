d25e5ed75488e8ebbaa0550a82b5d74292004adf
AEU <aeuplayground@gmail.com>
2025-08-25 20:21:29 +0900

    chore : 프로젝트 세팅

---
+++ src/main/java/forward/javaqna/JavaQnaApplication.java
@@ -0,0 +1,13 @@
+package forward.javaqna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaQnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaQnaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/forward/javaqna/JavaQnaApplicationTests.java
@@ -0,0 +1,13 @@
+package forward.javaqna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaQnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
