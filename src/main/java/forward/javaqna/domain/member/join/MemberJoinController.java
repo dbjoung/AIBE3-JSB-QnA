43d928cafe927db2805ae76fbf7a9f47ab98646d
AEU <aeuplayground@gmail.com>
2025-08-30 13:53:37 +0900

    :sparkles: feat : MemberJoin 컨트롤러, 서비스 단 구현 (회원 가입 관련 로직)

--- src/main/java/forward/javaqna/domain/member/join/MemberJoinController.java
+++ src/main/java/forward/javaqna/domain/member/join/MemberJoinController.java
@@ -1,10 +1,48 @@
 package forward.javaqna.domain.member.join;
 
-import forward.javaqna.domain.member.core.MemberRepository;
+import forward.javaqna.domain.member.core.Member;
+import forward.javaqna.domain.member.core.policy.MemberJoinPolicy;
+import forward.javaqna.domain.member.join.dto.SignUpFormDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/user/auth")
+@RequiredArgsConstructor
 @Controller
 public class MemberJoinController {
-    private MemberRepository memberRepository;
+    private final MemberJoinService memberJoinService;
 
+    @GetMapping("/signup")
+    public String viewSignup(@ModelAttribute(name = "form") SignUpFormDTO signUpForm) {
+        return "member/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(
+            @ModelAttribute(name = "form") @Valid SignUpFormDTO signUpForm,
+            BindingResult bindingResult,
+            Model model
+    ) {
+
+        if (!MemberJoinPolicy.isPasswordMatch(signUpForm.getPassword(), signUpForm.getPasswordConfirm()))
+            bindingResult.addError(new ObjectError("패스워드 불일치", "입력한 비밀번호가 다릅니다."));
+
+        if (!bindingResult.hasFieldErrors("username") && memberJoinService.hasSameUsername(signUpForm.getUsername()))
+            bindingResult.addError(new ObjectError("username 중복","이미 있는 사용자 아이디입니다."));
+
+        if (bindingResult.hasErrors()) return "member/signup";
+
+        Member newMember = memberJoinService.addMember(signUpForm);
+        model.addAttribute("member", newMember);
+
+        return "/member/signup_result";
+    }
 }
--- src/main/java/forward/javaqna/domain/member/join/MemberJoinService.java
+++ src/main/java/forward/javaqna/domain/member/join/MemberJoinService.java
@@ -1,9 +1,39 @@
 package forward.javaqna.domain.member.join;
 
+import forward.javaqna.domain.member.core.Member;
 import forward.javaqna.domain.member.core.MemberRepository;
+import forward.javaqna.domain.member.join.dto.SignUpFormDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class MemberJoinService {
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // 멤버 추가 함수
+    public Member addMember(SignUpFormDTO signUpFormDTO) {
+        signUpFormDTO.setPassword(encodePassword(signUpFormDTO.getPassword()));
+        return memberRepository.save(signUpFormDTO.toMember());
+    }
+
+    // 유저 네임 기반으로 회원 검색
+    public Member findMemberByUsername(String username) {
+        return memberRepository.findById(username).orElse(null);
+    }
+
+    //
+    public boolean hasSameUsername(String username) {
+        return findMemberByUsername(username) != null;
+    }
+
+    public String encodePassword(String password) {
+        return bCryptPasswordEncoder.encode(password);
+    }
+
+    public boolean checkPassword(String originalPassword, String encryptionPassword) {
+        return bCryptPasswordEncoder.matches(originalPassword, encryptionPassword);
+    }
 }
