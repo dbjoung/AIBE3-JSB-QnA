1c102ac151d430c849e6b81fad63c5d94232cd87
SeokHwan13 <seokhwan1113@naver.com>
2025-09-01 16:51:31 +0900

    :white_check_mark: test : 질문 보기 테스트 코드 수정 #6

--- src/test/java/forward/javaqna/domain/question/query/QuestionQueryServiceTests.java
+++ src/test/java/forward/javaqna/domain/question/query/QuestionQueryServiceTests.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
@@ -25,13 +28,17 @@ public class QuestionQueryServiceTests {
     @Autowired
     QuestionRepository questionRepository;
 
+    int testId;
+
     @BeforeEach
     public void setup() { //Question 엔티티에 @Setter 어노테이션 추가 후 테스트
         questionRepository.deleteAll();
         Question question = new Question();
         question.setTitle("Test Question 1");
         question.setContent("Test Question 1");
-        questionRepository.save(question);
+        Question a = questionRepository.save(question);
+        testId = a.getId();
+
         Question question1 = new Question();
         question1.setTitle("Test Question 2");
         question1.setContent("Test Question 2");
@@ -41,19 +48,22 @@ public class QuestionQueryServiceTests {
     @Test
     @DisplayName("질문 목록 조회")
     void t1() {
-        List<QuestionListDTO> questionList = questionQueryService.getQuestionList();
+        Pageable pageable = PageRequest.of(1, 5);
+        Page<QuestionListDTO> questionList = questionQueryService.getQuestionPaging(pageable);
 
-        for(QuestionListDTO questionListDTO : questionList){
-            System.out.println("질문 ID : " + questionListDTO.getId());
-        }
+        questionList.getContent().forEach(q ->
+                System.out.println("질문 ID : " + q.getId())
+        );
 
-        assertThat(questionList).hasSize(2);
+        assertThat(questionList.getContent()).hasSize(2);
     }
 
     @Test
     @DisplayName("질문 상세 조회")
     void t2() {
-        QuestionDTO questionDTO = questionQueryService.getQuestionById(1);
+        QuestionDTO questionDTO;
+        questionDTO = questionQueryService.getQuestionById(testId);
+        System.out.println("이거 왜 안댐 : " + questionDTO.getTitle());
         assertThat(questionDTO.getTitle()).isEqualTo("Test Question 1");
     }
 }
