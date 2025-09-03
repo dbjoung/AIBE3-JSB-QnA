191a91c4461926aaa65188db539af1ac62e5f1b7
AEU <aeuplayground@gmail.com>
2025-09-03 10:00:33 +0900

    :sparkles: feat : 검색용 임시 템플릿 작성, DTO 작성

--- src/main/java/forward/javaqna/domain/question/search/dto/SearchFormDTO.java
+++ src/main/java/forward/javaqna/domain/question/search/dto/SearchFormDTO.java
@@ -1,12 +1,23 @@
 package forward.javaqna.domain.question.search.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
 public class SearchFormDTO {
-    @NotBlank
-    String kwType;
-    @NotBlank
-    String keyword;
+    private String kwType;
+    private String keyword;
+
+    public SearchFormDTO(String kwType, String keyword) {
+        this.kwType = kwType;
+        this.keyword = keyword;
+    }
+
+    public boolean iskwTypeNull() {
+        return kwType == null;
+    }
+    public boolean iskeywordNull() {
+        return keyword == null;
+    }
 }
