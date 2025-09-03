31b9ea5d8e64582723fab27c74359f26340b3bc3
Do Ha Kim <129157326+ehgk4245@users.noreply.github.com>
2025-09-03 09:07:23 +0900

    답변 등록/수정/삭제 기능 입력 값 검증, Controller/뷰 작성, 세션 인증 연동 (#20)
    * :sparkles: feat : 쿼리 최적화
    
    * :sparkles: feat : 답변 등록/수정/삭제 view - controller - service 연동
    
    * :sparkles: feat : 권한별 리소스 UI 표시
    
    * :sparkles: feat : 입력 값 검증 추가
    
    * :recycle: refactor : 불필요한 코드 제거 (slf4j)

--- src/main/java/forward/javaqna/domain/answer/core/AnswerRepository.java
+++ src/main/java/forward/javaqna/domain/answer/core/AnswerRepository.java
@@ -1,8 +1,15 @@
 package forward.javaqna.domain.answer.core;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
 
     void deleteByQuestion_Id(Integer questionId);
+
+    @Query("select a from Answer a join fetch a.member where a.id = :id")
+    Optional<Answer> findByIdEager(@Param("id") Integer id);
 }
